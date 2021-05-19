006021c81fa31c72d64ea16ac16968bea2e00ba3
electrosoftech <83676779+electrosoftech@users.noreply.github.com>
2021-05-19 21:49:25 +0300

    Update leddriver.c

--- led_button_driver/Drivers/MODULES/leddriver.c
+++ led_button_driver/Drivers/MODULES/leddriver.c
@@ -1,9 +1,3 @@
-/*
- * sensor.c
- *
- *  Created on: Mar 18, 2021
- *      Author: Numan
- */
 
 #include "stm32f4xx_hal.h"
 void led_driver_init(void)
@@ -27,30 +21,19 @@ void led_driver_init(void)
 	GPIOD->OTYPER &= ~(1<<14);
 	GPIOD->OTYPER &= ~(1<<15);
 
-	GPIOD->OSPEEDR &= ~(1<<27);
-	GPIOD->OSPEEDR |= (1<<26);
-
-	GPIOD->OSPEEDR &= ~(1<<25);
-	GPIOD->OSPEEDR |= (1<<24);
-
-	GPIOD->OSPEEDR &= ~(1<<29);
-	GPIOD->OSPEEDR |= (1<<28);
-
-	GPIOD->OSPEEDR &= ~(1<<31);
-	GPIOD->OSPEEDR |= (1<<30);
 }
 void led_driver_on(void)
 {
- GPIOD->BSRR=(1<<12);
- GPIOD->BSRR=(1<<13);
- GPIOD->BSRR=(1<<14);
- GPIOD->BSRR=(1<<15);
+ GPIOD->BSRR|=(1<<12);
+ GPIOD->BSRR|=(1<<13);
+ GPIOD->BSRR|=(1<<14);
+ GPIOD->BSRR|=(1<<15);
 }
 void led_driver_off(void)
 {
- GPIOD->BSRR=(1<<28);
- GPIOD->BSRR=(1<<29);
- GPIOD->BSRR=(1<<30);
- GPIOD->BSRR=(1<<31);
+ GPIOD->BSRR|=(1<<28);
+ GPIOD->BSRR|=(1<<29);
+ GPIOD->BSRR|=(1<<30);
+ GPIOD->BSRR|=(1<<31);
 }
 
